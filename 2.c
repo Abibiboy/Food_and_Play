1b04c8d9b8d25fa780a7e7d5dd45f24d67ec6735
Abibiboy <93177813+Abibiboy@users.noreply.github.com>
2021-10-30 23:35:51 +0800

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,28 @@
+//#include<stdio.h>
+//#include<math.h>
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//int main()
+//{
+//	int i=0;
+//	int j=0;
+//	int count=0;
+//	int mid=0;
+//	scanf("%d",&i);
+//	mid=i;
+//	//判断这个整数是几位数
+//	for(count=1;mid/10!=0;count++)
+//	{
+//		mid/=10;
+//	}
+//	printf("这是个%d位数\n",count);
+//
+//	for(j=2;j<=sqrt(i);j++)
+//	{
+//		if(i%j==0)
+//			break;
+//	}
+//	if(j>sqrt(i)) printf("%d是个素数\n",i);
+//}
+//				
+//
